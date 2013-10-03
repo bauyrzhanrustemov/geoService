67abb5d3b076b07363a09c4c0814bea5805e9dfc
bauyrzhanrustemov <baur_chik@mail.ru>
2013-10-03 17:10:35 +0600

    sdsad

---
+++ geoService/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace geoService
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
